fb590363c709fd5b612e3248dabc8e625eab3531
HagaSpa <justicesparrow13@gmail.com>
2019-11-24 12:10:52 +0900

    testとコメント追記

--- 9cc.c
+++ 9cc.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * 与えられた1つの引数を、プログラムの終了コードするアセンブリを出力する。
+ * 
+ * argc: コマンドライン引数の個数
+ * **argv: argvはコマンドライン引数を格納した配列
+ *          **argvなので配列のポインタ変数のポインタ？
+ */
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+  
+  # 2つめの引数を./9ccバイナリに渡して実行し、その結果（=アセンブリ）をtmp.sへ書き込んでいる
+  ./9cc "$input" > tmp.s
+  # tmp.sをtmpバイナリへアセンブル。アセンブリ➡️機械語
+  gcc -o tmp tmp.s
+  ./tmp
+  # shは$?で直前のコードの終了コードを取得できる（ここでは./tmpの終了コード）
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
