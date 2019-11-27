a31260637fac5be804d2a29722a6bee7b478021d
HagaSpa <justicesparrow13@gmail.com>
2019-11-27 22:49:29 +0900

    加減算に対するトークナイザを実装

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,118 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// tokenの種類
+// enum型にTokenKindという名前でエイリアスしている
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 数値
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// エラーを報告する関数
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// 次のトークンが期待する記号の時は、トークンを1つ進めてtrueを返す。
+// それ以外はfalseを返す
+bool consume(char op) {
+  // kindがTK_RESERVEDじゃない場合 or 一番最初の文字じゃない場合。
+  // 「+5-4]みたいな式の場合「-」のみ？
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待する記号の時は、トークンを1つ読み進める。
+// それ以外の場合はエラーを報告する。
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c'ではありません。", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の時は、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合はエラーを報告する。
+int expect_number() {
+  if (token->kind != TK_NUM) {
+    error("数ではありません。");
+  }
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+// EOFかどうか
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列をトークナイズして、新しいトークンを返却する
+Token *tokenize(char *p) {
+  // 最初のトークンを初期化
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白の場合読み飛ばす
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // 記号の場合
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    // 数値の場合
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("invalid token");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
+
 
 /**
  * 与えられた加減算のみの式を、実行するアセンブリを生成する
@@ -10,36 +123,32 @@
  */
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません\n");
+    error("%s: invalid number of arguments", argv[0]);
     return 1;
   }
 
-  // char型のポインタ変数
-  char *p = argv[1];
+  // トークナイズ実行
+  token = tokenize(argv[1]);
 
+  // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  // 引数の最初の数字部分（文字列）を10進数のlong型として取得
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      // ポインタを進めている
-      p++; 
-      // +の後は数字を想定.
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+
+  // 式の最初は数字じゃなくてはならないので、それをチェック
+  // 最初のmov命令を実行
+  printf("  mov rax, %d\n", expect_number());
+
+  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ、アセンブリ出力
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "予期しない文字列です: '%c'\n", *p);
-    return 1;
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
   }
 
   printf("  ret\n");
