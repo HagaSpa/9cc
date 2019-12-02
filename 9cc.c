19b8add51ccce5808686e6c7ac875924524103de
HagaSpa <justicesparrow13@gmail.com>
2019-12-02 22:30:02 +0900

    Implement unary plus and minus

--- 9cc.c
+++ 9cc.c
@@ -176,6 +176,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -192,18 +193,28 @@ Node *expr() {
   }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+// unary = ("+" | "-")? unary | primary
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    // 負の数の場合は、左辺に0を入れて0-xとして表現
+    return new_node(ND_SUB, new_node_num(0), unary());
+  return primary();
 }
 
 // primary = num | "(" expr ")"
